dcc0ca2f5406cefd59f306021dbfc17b16dc6246
OwenDurkin <owen@thedurks.com>
2021-06-29 16:24:40 -0400

    implement rudimentary tree drawing

--- src/index.js
+++ src/index.js
@@ -7,6 +7,40 @@ const SCREEN_HEIGHT = 1000;
 const RADIUS = 10;
 const STROKE_WIDTH = 4;
 
+// FUNCTIONS FOR TREE DRAWING
+
+// crappy thing I came up with based on skimming that one article
+// http://llimllib.github.io/pymag-trees/
+const bfsPosCalc = (edgeMap, nodeCount) => {
+    const positions = Array(nodeCount);
+    const queue = [[0,0]];
+    let trackedLevel = 0;
+    let [x,y] = [30,30]
+    while(queue.length > 0) {
+        const [curNode, curLevel] = queue.shift();
+        if(curLevel > trackedLevel) {
+            trackedLevel = curLevel;
+            x = 30;
+            y += 30;
+        }
+        positions[curNode] = [x,y];
+        x += 30;
+        const children = edgeMap[curNode];
+        if (children === undefined) {
+            continue;
+        }
+        for (let i = 0; i < children.length; i++) {
+            const child = children[i];
+            queue.push([child,curLevel+1]);
+        }
+    }
+    return positions;
+}
+
+
+// ACTUAL REACT COMPONENTS
+
+// props: x,y
 const Node = (props) => (
     <circle
         cx={props.x}
@@ -16,26 +50,40 @@ const Node = (props) => (
     />
 );
 
-
+// props:
+// => tree (i.e. an edge map, with 0 as root)
+// => nodeCount, the number of nodes
 const Tree = (props) => {
-    const nodes = Array(20).fill().map((undef,i) =>
-        (<Node
-            key={i}
-            x={20}
-            y={30*(i+1)}
-        />)
-    );
-    const edges = Array(19).fill().map((undef,i) =>
-        (<line
-            key={i}
-            x1={nodes[i].props.x}
-            y1={nodes[i].props.y}
-            x2={nodes[i+1].props.x}
-            y2={nodes[i+1].props.y}
-            stroke="black"
-            strokeWidth={STROKE_WIDTH}
-        />)
-    );
+    // transforms a tree into a list of x,y coordinates, one for each node
+    const calculatePositions = bfsPosCalc;
+
+    const nodePositions = calculatePositions(props.edgeMap, props.nodeCount);
+    const nodes = []
+    for (let i = 0; i < props.nodeCount; i++) {
+        const [x,y] = nodePositions[i];
+        nodes.push(
+            (<Node key={i} x={x} y={y} />)
+        );
+    }
+    const edges = [];
+    for(let i = 0; i < props.nodeCount; i++) {
+        const curEdges = props.edgeMap[i];
+        if (curEdges==undefined) {
+            continue;
+        }
+        for(let j = 0; j < curEdges.length; j++) {
+            const c = curEdges[j];
+            edges.push((<line
+                key={[i,j]}
+                x1={nodePositions[i][0]}
+                y1={nodePositions[i][1]}
+                x2={nodePositions[c][0]}
+                y2={nodePositions[c][1]}
+                stroke="black"
+                strokeWidth={STROKE_WIDTH}
+            />));
+        }
+    }
     return (
         <svg width={SCREEN_WIDTH} height={SCREEN_HEIGHT}>
             {nodes}
@@ -46,7 +94,12 @@ const Tree = (props) => {
 
 ReactDOM.render(
   <React.StrictMode>
-    <Tree/>
+    <Tree nodeCount={9} edgeMap={{
+        0: [1,2],
+        1: [3,4],
+        2: [5,6],
+        3: [7,8],
+    }}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
