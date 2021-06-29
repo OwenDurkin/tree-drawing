b380a95989e20b3ddc6d4ddd206d29b9fbe247b6
OwenDurkin <owen@thedurks.com>
2021-06-29 15:39:30 -0400

    delete starter code; start drawing stuff

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const SCREEN_WIDTH = 1618;
+const SCREEN_HEIGHT = 1000;
+
+const RADIUS = 10;
+const STROKE_WIDTH = 4;
+
+const Node = (props) => (
+    <circle
+        cx={props.x}
+        cy={props.y}
+        r={RADIUS}
+        style={{fill:"black",stroke:"black"}}
+    />
+);
+
+
+const Tree = (props) => {
+    const nodes = Array(20).fill().map((undef,i) =>
+        (<Node
+            key={i}
+            x={20}
+            y={30*(i+1)}
+        />)
+    );
+    const edges = Array(19).fill().map((undef,i) =>
+        (<line
+            key={i}
+            x1={nodes[i].props.x}
+            y1={nodes[i].props.y}
+            x2={nodes[i+1].props.x}
+            y2={nodes[i+1].props.y}
+            stroke="black"
+            strokeWidth={STROKE_WIDTH}
+        />)
+    );
+    return (
+        <svg width={SCREEN_WIDTH} height={SCREEN_HEIGHT}>
+            {nodes}
+            {edges}
+        </svg>
+    );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Tree/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
