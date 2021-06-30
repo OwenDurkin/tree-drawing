20daeff747448c6c6485f805612a663ae8ebb268
OwenDurkin <owen@thedurks.com>
2021-06-29 20:59:14 -0400

    GROSS TMP COMMIT; experimenting with quadratic solutions

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-const SCREEN_WIDTH = 500;
-const SCREEN_HEIGHT = 200;
+const SCREEN_WIDTH = 400;
+const SCREEN_HEIGHT = 300;
 
 const RADIUS = 10;
 const STROKE_WIDTH = 4;
@@ -21,6 +21,12 @@ const SCALE = RADIUS+20;
 //
 // Reingold-Tilford Algorithm in-depth
 // https://rachel53461.wordpress.com/2014/04/20/algorithm-for-drawing-trees/
+//
+// Buchheim algorithm
+// http://dirk.jivas.de/papers/buchheim02improving.pdf
+//
+// Brown University Paper
+// https://cs.brown.edu/people/rtamassi/gdhandbook/chapters/trees.pdf
 
 
 // GRAPH UTILITIES
@@ -50,6 +56,47 @@ const edgeMapToRootNode = (edgeMap) => {
     }
     return root;
 }
+/*
+// contours are the leftmost (rightmost) x coordinate of a node on a given level of a tree
+const contour = (root, maxlevel, comparator) {
+    let cont = [];
+    const calcCont = (node, comparator, depth=0) => { 
+        if (cont.length===0) {
+            cont = [node.x];
+        }
+        else if (cont.length < level+1) {
+            cont.push(node.x);
+        }
+        else if (comparator(cont[level], node.x)) {
+            cont[level] = node.x
+        }
+        for (const child of node.children) {
+            calcCont(child, comparator, level+1, cont);
+        }
+    }
+    calcCont(root,comparator,0);
+    return cont
+}
+*/
+
+
+// for determining how far a subtree needs to be moved to the right
+/*
+const push_right = (leftNode, rightNode) => {
+    leftContour = contour(leftNode, (x,y) => (x < y)); 
+    rightContour = contour(rightNode, (x,y) => (x > y));
+    let max = 0;
+    leftContour.forEach((lx,i) => {
+        ly = rightContour[i];
+        max = Math.max(lx-ly,max);
+    })
+    return max + 1;
+}
+*/
+
+// for use with thread-involved algos;
+// 
+// threads in this context have nothing to do with multithreading
 
 
 // TREE DRAWING METHODS
@@ -146,6 +193,106 @@ const parentBasedPosCalc = (edgeMap,nodeCount) => {
 }
 
 
+const NODE_SEP = 1;
+const quadraticPosCalc = (edgeMap, nodeCount) => {
+    const positions = Array(nodeCount);
+    const root = edgeMapToRootNode(edgeMap);
+    const moveRight = (node, mod) =>  {
+        node.x += mod;
+        if(node.children) {
+            for(const child of node.children) {
+                moveRight(child, mod);
+            }
+        }
+    }
+    const getSidemost = (node,comp) => {
+        if (node.children) {
+            return node.children.reduce(
+                (acc,nextChild) => {
+                    acc = comp(acc,getSidemost(nextChild,comp));
+                    return acc;
+                },
+                node.x
+            );
+        }
+        return node.x;
+    }
+    const width = (node) => (getSidemost(node,Math.max)-getSidemost(node,Math.min));
+    const calcPos = (node) => {
+        if(node.children) {
+            node.children.forEach((child,i) => {
+                child.x = 0;
+                calcPos(child);
+                if(i > 0) {
+                    const prev = node.children[i-1];
+                    const offset = getSidemost(prev,Math.max)+NODE_SEP;
+                    moveRight(child,offset);
+                }
+                node.x = (node.children[0].x + node.children[node.children.length-1].x)/2;
+            });
+        }
+    }
+    const applyPos = (node,depth=0) => {
+        positions[node.id] = [SCALE*(node.x+1),SCALE*(depth+1)];
+        if(node.children) {
+            for(const child of node.children) {
+                applyPos(child,depth+1);
+            }
+        }
+    }
+    calcPos(root);
+    applyPos(root);
+    return positions;
+}
+
+
+/*
+const reingoldTilfordPosCalc = (edgeMap,nodeCount) => {
+    const positions = Array(nodeCount);
+    const root = edgeMapToRootNode(edgeMap);
+    // assign 0 to leftmost nodes, shift by 1 if it has a left sibling
+    // center children over parents
+    const initialSetup = (node) => {
+        if(node.children) {
+            // arbitrary starting x; handles base case
+            node.children.forEach((child,i) => {
+                child.x = NODE_SEP*i;
+            });
+            // determine the subtrees; shift as necessary
+            let mod = 0;
+            let [leftmost, rightmost] = [0,0];
+            node.children.forEach((child,i) => {
+                const [subleftmost, subrightmost] = initialSetup(child);
+                if (mod+rightmost <= child.x-subleftmost) {
+                    mod += SUBTREE_SEP + (child.x-(mod+rightmost));
+                    child.mod = mod;
+                }
+                rightmost = child.x+mod+subrightmost;
+            });
+            // center the parent over the children 
+            node.x = (rightmost-leftmost)/2
+            // return the left and right countours
+            return [leftmost,rightmost];
+        } else {
+            return [node.x,node.x];
+        }
+    }
+    // apply shifts in O(n) time throughout the tree
+    const modify = (node, depth=0, mod=0) => {
+        positions[node.id] = [SCALE*(node.x+mod+1),SCALE*(depth+1)];
+        if(node.children) {
+            node.children.forEach((child,i) => {
+                modify(child,depth+1,mod+node.mod);
+            });
+        }
+    }
+    // execute the algorithm
+    root.x = 0;
+    initialSetup(root);
+    modify(root);
+    return positions;
+}
+*/
 
 // ACTUAL REACT COMPONENTS
 
@@ -201,7 +348,7 @@ const TreeDrawing = (props) => {
 
 
 const Forest = (props) => {
-    const methods = [thinPosCalc, knuthPosCalc,parentBasedPosCalc];
+    const methods = [thinPosCalc, knuthPosCalc,parentBasedPosCalc,quadraticPosCalc];
     const drawings = methods.map((method,i) => (
         <div className="Column" key={i}>
             <h1>{method.name}</h1>
@@ -222,6 +369,9 @@ const Forest = (props) => {
 
 
 const TREES = [
+    [{ // full binary tree of size 3
+        0: [1,2],
+    }, 3],
     [{ // simple tree
         0: [1,2],
         1: [3,],
@@ -248,7 +398,12 @@ const TREES = [
         0: [1,2],
         2: [3],
         3: [4],
-    }, 5],
+        4: [5],
+        5: [6],
+        6: [7],
+        7: [8],
+        8: [9]
+    }, 10],
     [{ // Dr. Dobbs
         0: [1,2,3],
         1: [4,5],
@@ -256,6 +411,13 @@ const TREES = [
         3: [8,9],
         9: [10,11,12,13,14],
     }, 15],
+    [{ // wide boi
+        0: [1,2],
+        1: [3],
+        3: [4,5,6,7,8],
+        2: [9,10,11,12],
+        12: [13,14,15,16,17],
+    },18],
 ];
 
 const alltreedrawings = TREES.map((tree,i) => (
